b11e3c0691fa72004ebd9e89f14ab1d483ce56ce
Adam Ali <adamstar080904@gmail.com>
2023-08-08 18:09:59 -0600

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Hello Friend</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex impedit
+        nesciunt totam laborum corrupti tempore sunt, quasi molestias voluptatum
+        assumenda fugit ea, iure et, delectus libero est? Ullam, ratione.
+        Possimus!
+      </p>
+    </>
+  );
+}
+
+export default App;
